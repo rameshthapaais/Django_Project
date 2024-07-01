0aa5eafdefe0682e194f5afd522f7dc0cc2a900c
rameshthapaais <rath842@ess.ais.ac.nz>
2024-07-01 17:03:04 +1200

    Added integrated test

--- chatrooms/urls.py
+++ chatrooms/urls.py
@@ -1,7 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from .viewsets import *
+from .views import *
+from django.urls import path
 
 router = DefaultRouter()
 router.register('chatroom', ChatRoomViewsets, basename='chatroom')
 router.register('message', MessageViewsets, basename='message')
 urlpatterns = router.urls
+urlpatterns += [
+    path('sum_num/', sumNumbersView, name='sum_num'),
+]
