b840f7abcec2dc2580508cd2c8e83e515ff1b928
rameshthapaais <rath842@ess.ais.ac.nz>
2024-07-08 17:10:58 +1200

    Token authentication is done

--- Project/settings.py
+++ Project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chatrooms',
-    'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
 
 ]
